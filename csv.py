4b6b1153b0c792ffdb41be3249b6989707b18588
Amadu1602 <64605178+Amadu1602@users.noreply.github.com>
2022-05-26 22:56:57 +0000

    commit

--- csv.py
+++ csv.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 from pyodide.http import open_url
 
-df = pd.read_csv(open_url("https://raw.gitbuusercontent.com/mwaskom/seaborn-data/master/diamonds.csv"))
+df = pd.read_csv(open_url("https://raw.gitbusercontent.com/mwaskom/seaborn-data/master/diamonds.csv"))
 
 csv = Element('csv')
 
