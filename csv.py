f8a45bb800f1561a7e7b5fb2e7146504a3304e28
Amadu1602 <64605178+Amadu1602@users.noreply.github.com>
2022-05-30 14:29:14 +0000

    commit changes

--- csv.py
+++ csv.py
@@ -5,7 +5,7 @@ from panel.io.pyodide import show
 
 
 
-fileInput = pn.widgets.FileInput(accept='.csv')
+fileInput = pn.widgets.FileInput(accept=['.csv', '.txt', '.zip'])
 uploadButton = pn.widgets.Button(name='Upload', button_type = 'primary')
 
 table = pn.widgets.Tabulator(pagination='remote', page_size=100)
