2baecda488b9ef18ab46804da0878d5cb62a7daf
Amadu Adam <amadu1602@gmail.com>
2022-06-19 21:19:29 +0000

    commit changes

--- data.py
+++ data.py
@@ -1,16 +1,21 @@
 
 import time
 from datetime import datetime 
+import sys
 
 
 print("Hello Amadu")
+
 # while True:
 #     now = datetime.now()
-#     print(now.strftime("%m/%d/%Y %H:$M:%S"))
+#     print ("%s/%s/%s %s:%s:%s" % (now.month,now.day,now.year,now.hour,now.minute,now.second)),
+#     sys.stdout.flush()
+#     print("\r"),
 #     time.sleep(1)
-while True:
-    now=datetime.now()
-    print(now.strftime("%d/%m/%Y %H:%M:%S"))
-    # print("\r", end="", flush=True)
+
+while True:   
+    now = datetime.now()
+    print (now.strftime("%m/%d/%Y %H:%M:%S"), end="", flush=True),
+    # print("\r", end="", flush=True),
     time.sleep(1)
 
