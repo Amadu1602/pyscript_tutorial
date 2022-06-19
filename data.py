a49c0f6255eafb9cf91ea875384e043d1a44bf7b
Amadu Adam <amadu1602@gmail.com>
2022-06-19 21:29:18 +0000

    commit changes

--- data.py
+++ data.py
@@ -13,9 +13,9 @@ print("Hello Amadu")
 #     print("\r"),
 #     time.sleep(1)
 
-while True:   
-    now = datetime.now()
-    print (now.strftime("%m/%d/%Y %H:%M:%S"), end="", flush=True),
-    # print("\r", end="", flush=True),
-    time.sleep(1)
+# while True:   
+#     now = datetime.now()
+#     print (now.strftime("%m/%d/%Y %H:%M:%S"), end="", flush=True),
+#     # print("\r", end="", flush=True),
+#     time.sleep(1)
 
